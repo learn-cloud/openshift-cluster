d08a0f2eb4b4d68849858f01c8b135dc7dfa6df9
learn-cloud <51190838+learn-cloud@users.noreply.github.com>
2021-11-11 13:10:26 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,3 +1,12 @@
+# terraform {
+#   required_version = ">= 0.12"
+# }
+
 terraform {
-  required_version = ">= 0.12"
+  required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = "1.30.0"
+    }
+  }
 }
