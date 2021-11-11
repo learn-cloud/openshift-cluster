e500b715a4e04ec61f36d9629a922040797027de
learn-cloud <51190838+learn-cloud@users.noreply.github.com>
2021-11-11 14:29:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,10 +2,10 @@ variable "machine_type" {
    default = "b3c.4x16"
 }
 
-variable "resource_group_id" {
-  type        = string
-  default     = "aadb17b59af948699a4b2bc66c1cda5c"
-}
+# variable "resource_group_id" {
+#   type        = string
+#   default     = "aadb17b59af948699a4b2bc66c1cda5c"
+# }
 
 variable "hardware" {
    default = "shared"
@@ -20,11 +20,11 @@ variable "default_pool_size" {
 }
 
 variable "private_vlan_id" {
-  default = "2063555-864-fcr01a.dal10"
+  default = "2777838-1667-bcr01a.dal10-vlan-us46"
 }
 
 variable "public_vlan_id" {
-  default = "2777838-1667-bcr01a.dal10-vlan-us46"
+  default = "2063555-864-fcr01a.dal10"
 }
 
 variable "cluster_name" {
