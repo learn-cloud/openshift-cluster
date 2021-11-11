f15939460d255fc500081dcd93d29303ccd7b98e
learn-cloud <51190838+learn-cloud@users.noreply.github.com>
2021-11-11 13:28:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@
 resource "ibm_container_cluster" "cluster" {
   name              = "${var.cluster_name}${random_id.name.hex}"
   datacenter        = var.datacenter
+  resource_group_id = var.resource_group_id
   default_pool_size = var.default_pool_size
   machine_type      = var.machine_type
   hardware          = var.hardware
